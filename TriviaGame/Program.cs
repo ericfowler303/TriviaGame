bcbabbdfbca99909072966e98b2727014180b743
Eric Fowler <ericfowler303@gmail.com>
2014-11-11 17:28:28 -0700

    Complete Trivia Class
    includes some commented out debugging code

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -11,6 +11,11 @@ namespace TriviaGame
     {
         static void Main(string[] args)
         {
+            /*Trivia q = new Trivia("Lyrics: making love to you was never second best*melt with you");
+            Console.WriteLine(q.Category);
+            Console.WriteLine(q.Question);
+            Console.WriteLine(q.Answer);
+            Console.ReadKey(); */
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
         }
@@ -30,6 +35,10 @@ namespace TriviaGame
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
             //Return the full list of trivia questions
+            foreach (string text in contents)
+            {
+                returnList.Add(new Trivia(text));
+            }
             return returnList;
         }
     }
@@ -40,13 +49,23 @@ namespace TriviaGame
 
         //The Trivia Object will have 2 properties
         // at a minimum, Question and Answer
-
+        public string Category { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
         //The Constructor for the Trivia object will
         // accept only 1 string parameter.  You will
         // split the question from the answer in your
         // constructor and assign them to the Question
         // and Answer properties
 
-
+        public Trivia(string garbledString)
+        {
+            if (garbledString.Contains(":"))
+            {
+                this.Category = garbledString.Split(':').First().ToString();
+            }
+            this.Question = garbledString.Split(':').Last().Split('*').First().Trim();
+            this.Answer = garbledString.Split('*').Last();
+        }
     }
 }
